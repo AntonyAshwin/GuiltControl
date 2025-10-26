774bcc8be65ce982b73ede54e35b318429705005
Antony Ashwin <antony.ashwin@sap.com>
2025-10-26 21:25:35 +0530

    Initial Commit

---
+++ The Guilt Control/The_Guilt_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Guilt_ControlApp.swift
+//  The Guilt Control
+//
+//  Created by Ashwin, Antony on 26/10/25.
+//
+
+import SwiftUI
+
+@main
+struct The_Guilt_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The Guilt ControlTests/The_Guilt_ControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  The_Guilt_ControlTests.swift
+//  The Guilt ControlTests
+//
+//  Created by Ashwin, Antony on 26/10/25.
+//
+
+import Testing
+@testable import The_Guilt_Control
+
+struct The_Guilt_ControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
